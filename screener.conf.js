fd0fa5041fbd6d3a749b0bb31ee04ec5a9f54c9b
molly-driscoll <molly.driscoll@saucelabs.com>
2019-09-11 11:05:23 -0500

    RDC

---
+++ screener.conf.js
@@ -0,0 +1,12 @@
+module.exports = {
+    projectRepo: 'user/my-project-repo',
+
+    apiKey: process.env.SCREENER_API_KEY,
+
+    states: [
+        {
+            url: 'https://www.saucedemo.com',
+            name: 'Name of UI State'
+        }
+    ]
+};
